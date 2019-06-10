5c020058b334e85c27fe6e0d746510c49bb70e22
EvinK <evink@foxmail.com>
2019-06-10 11:06:10 +0800

    update

--- demo/assets/script/js/slider.js
+++ demo/assets/script/js/slider.js
@@ -1,8 +1,8 @@
 "use strict";
 function $(selector) {
-    var element;
-    var firstChar = selector.substr(0, 1);
-    var mainSelector = selector.substr(1);
+    let element;
+    let firstChar = selector.substr(0, 1);
+    let mainSelector = selector.substr(1);
     switch (firstChar) {
         case '#':
             element = document.getElementById(mainSelector);
@@ -24,55 +24,88 @@ function $(selector) {
     }
     return element;
 }
-var Slider = (function () {
-    function Slider(options) {
+class IMGArray {
+    constructor() {
+        this._arr = [];
+    }
+    get arr() {
+        console.log('GETTER');
+        return this._arr;
+    }
+    set arr(value) {
+        console.warn('set function has been banned for IMGArray');
+    }
+    push(value) {
+        console.log(1111);
+        return this._arr.push(value);
+    }
+    pop() {
+        return this._arr.pop();
+    }
+}
+class Slider {
+    constructor(options) {
         this.options = {
-            address: ['http://ov998a2gm.bkt.clouddn.com/lib/pictures/content_1.jpg',
-                'http://ov998a2gm.bkt.clouddn.com/lib/pictures/content_2.jpg',
-                'http://ov998a2gm.bkt.clouddn.com/lib/pictures/bgimg_3.jpg',
-                'http://ov998a2gm.bkt.clouddn.com/lib/pictures/bgimg_4.jpg'],
+            address: ['http://evink.jscli.org/lib/pictures/content_1.jpg',
+                'http://evink.jscli.org/lib/pictures/content_2.jpg',
+                'http://evink.jscli.org/lib/pictures/bgimg_3.jpg',
+                'http://evink.jscli.org/lib/pictures/bgimg_4.jpg'],
             method: 'random',
             containerId: 'body',
             duration: 1,
             delay: 3000,
             indicatorStyle: 'dot',
             customIndicator: false,
-            indicatorCodes: ['box-shadow:0 0 5px 2px red; opacity:0.8'],
+            indicatorCodes: ['box-shadow:0 0 5px 2px red opacity:0.8'],
             hasClick: false,
             callback: []
         };
         this.components = {};
+        this.imgsComponents = [];
         this.styles = {
-            imgBox: "#slider-img-box {\n                    position: relative;\n                    width: inherit;\n                    height: inherit;\n                    // overflow: hidden;\n                }",
-            imgContainers: "#slider-img-box .slider-img-containers {\n                        position: absolute;\n                        top: 0;\n                        left: 0;\n                        width: inherit;\n                        height: inherit;\n                    }",
-            imgs: "#slider-img-box .slider-img-containers .slider-img {\n                    width: inherit;\n                    height: inherit;\n                }",
+            imgBox: `#slider-img-box {
+                    position: relative;
+                    width: inherit;
+                    height: inherit;
+                   /* overflow: hidden; */
+                }`,
+            imgContainers: `#slider-img-box .slider-img-containers {
+                        position: absolute;
+                        top: 0;
+                        left: 0;
+                        width: inherit;
+                        height: inherit;
+                    }`,
+            imgs: `#slider-img-box .slider-img-containers .slider-img {
+                    width: inherit;
+                    height: inherit;
+               }`,
         };
+        this.body = document.body;
         this.options = Object.assign(this.options, options);
         this.init();
         this.slide();
     }
-    Slider.prototype.init = function () {
+    init() {
         this.generateStyleSheet();
-        var imgs;
+        let imgs;
         imgs = this.options.address;
         if (imgs === (null || undefined))
             throw new DOMException('can`t get img elements, Slider.js init failure.');
-        var body;
         if (this.options.containerId === 'body')
-            body = $('body')[0];
+            this.body = window.document.body;
         else
-            body = $('#' + this.options.containerId);
-        var imgBox = document.createElement('div');
+            this.body = $('#' + this.options.containerId);
+        const imgBox = document.createElement('div');
         imgBox.id = 'slider-img-box';
-        var i = 1;
-        var imgContainers = new Array();
-        var imgElements = new Array();
-        for (var _i = 0, imgs_1 = imgs; _i < imgs_1.length; _i++) {
-            var value = imgs_1[_i];
-            var img = this.generateImg({
-                containerId: "slider-img-container-" + i,
-                containerClasses: ["slider-img-containers"],
-                id: "slider-img-" + i,
+        let i = 5;
+        let imgContainers = new Array();
+        let imgElements = new Array();
+        for (let value of imgs) {
+            let img = this.generateImg({
+                containerId: `slider-img-container-${i}`,
+                containerClasses: [`slider-img-containers`],
+                id: `slider-img-${i}`,
                 classes: ['slider-img'],
                 src: value,
                 farther: imgBox
@@ -80,48 +113,33 @@ var Slider = (function () {
             i++;
             imgContainers.push(img[0]);
             imgElements.push(img[1]);
+            this.imgsComponents.push(img[0]);
         }
         this.components.startImg = imgElements[0];
         this.components.startImgContainer = imgContainers[0];
-        for (var _a = 0, imgs_2 = imgs; _a < imgs_2.length; _a++) {
-            var value = imgs_2[_a];
-            var img = this.generateImg({
-                containerId: "slider-img-container-" + i,
-                containerClasses: ["slider-img-containers"],
-                id: "slider-img-" + i,
-                classes: ['slider-img'],
-                src: value,
-                farther: imgBox
-            }, i, imgs.length);
-            i++;
-            imgContainers.push(img[0]);
-            imgElements.push(img[1]);
-        }
         this.components.endImg = imgElements[imgElements.length - 1];
         this.components.endImgContainer = imgContainers[imgContainers.length - 1];
-        body.appendChild(imgBox);
+        this.body.appendChild(imgBox);
         this.components.imgBox = imgBox;
         this.components.imgContainers = imgContainers;
         this.components.imgs = imgElements;
-    };
-    Slider.prototype.generateImg = function (attributes, i, imgsLength) {
-        var originalIndex = i;
-        var container = document.createElement('div');
+    }
+    generateImg(attributes, i, imgsLength) {
+        let originalIndex = i;
+        let container = document.createElement('div');
         container.id = attributes.containerId;
-        for (var _i = 0, _a = (attributes.containerClasses); _i < _a.length; _i++) {
-            var claz = _a[_i];
-            container.className += claz + " ";
+        for (let claz of (attributes.containerClasses)) {
+            container.className += claz + ` `;
         }
-        var img = document.createElement('img');
+        let img = document.createElement('img');
         img.id = attributes.id;
-        for (var _b = 0, _c = (attributes.classes); _b < _c.length; _b++) {
-            var claz = _c[_b];
-            img.className += claz + " ";
+        for (let claz of (attributes.classes)) {
+            img.className += claz + ` `;
         }
         img.src = attributes.src;
         container.appendChild(img);
         attributes.farther.appendChild(container);
-        var displaceMent;
+        let displaceMent;
         if (i >= imgsLength) {
             i = i - imgsLength - 1;
             displaceMent = i * 100;
@@ -129,53 +147,56 @@ var Slider = (function () {
         else {
             displaceMent = (imgsLength - i + 1) * -100;
         }
-        var style = " #slider-img-box #" + container.id + " {\n                        left: " + displaceMent + "%;\n                    }";
+        let style = ` #slider-img-box #${container.id} {
+                        left: ${displaceMent}%
+                    }`;
         if (originalIndex != 1) {
-            var keyframe = " @keyframes slide-" + originalIndex + " {\n                0% {\n                    left: " + displaceMent + "%;\n                }\n                100% {\n                    left: " + (displaceMent - 100) + "%;\n                }\n            }";
+            let keyframe = ` @keyframes slide-${originalIndex} {
+                0% {
+                    left: ${displaceMent}%;
+                }
+                100% {
+                    left: ${displaceMent - 100}%;
+                }
+            }`;
             this.appendToSheet(keyframe);
         }
         this.appendToSheet(style);
         return [container, img];
-    };
-    Slider.prototype.generateStyleSheet = function () {
-        var parent = $('html')[0];
-        var body = $('body')[0];
-        var sheet = document.createElement('style');
+    }
+    generateStyleSheet() {
+        const html = $('html')[0];
+        let sheet = document.createElement('style');
         sheet.id = 'slider-sheet';
         sheet.className = 'slider-sheet';
-        parent.insertBefore(sheet, body);
+        html.insertBefore(sheet, document.body);
         this.components.sheet = sheet;
         this.appendToSheet(this.styles.imgBox);
         this.appendToSheet(this.styles.imgContainers);
         this.appendToSheet(this.styles.imgs);
         return sheet;
-    };
-    Slider.prototype.appendToSheet = function (style) {
+    }
+    appendToSheet(style) {
         this.components.sheet.innerHTML += style;
         this.components.sheet.innerHTML += '\n';
-    };
-    Slider.prototype.addInAnimations = function (originalIndex) {
-    };
-    Slider.prototype.addOutAnimations = function (originalIndex) {
-    };
-    Slider.prototype.slide = function () {
-        var _this = this;
-        setInterval(function () {
-            _this.containerSlideEvent();
+    }
+    addInAnimations(originalIndex) {
+    }
+    addOutAnimations(originalIndex) {
+    }
+    slide() {
+        console.log('slide');
+        setInterval(() => {
+            console.log(1);
+            this.containerSlideEvent();
         }, 1000);
-    };
-    Slider.prototype.containerSlideEvent = function () {
-        var containers = this.components.imgContainers;
-        var imgLength = containers.length;
-        var i = 0;
-        for (var _i = 0, containers_1 = containers; _i < containers_1.length; _i++) {
-            var container = containers_1[_i];
-            var id = container.id;
-            this.appendToSheet("#" + id + "{\n                animation: slide-" + (i + 1) + " 1s;\n                animation-fill-mode: forwards;\n            }");
-            i++;
-        }
-    };
-    return Slider;
-}());
-var opts = {};
-var s = new Slider(opts);
+    }
+    containerSlideEvent() {
+        let containers = this.components.imgContainers;
+        console.log(containers[0]);
+    }
+    imgsOnChange(img) {
+    }
+}
+let opts = {};
+let arr = [];
--- demo/assets/script/ts/Slider.ts
+++ demo/assets/script/ts/Slider.ts
@@ -1,6 +1,31 @@
 /// <reference path="utils.ts" />
 
-// import { $ } from "./utils"; module = commonjs
+// import { $ } from "./utils" module = commonjs
+
+
+class IMGArray {
+    private _arr: Array<any> = []
+
+    get arr() {
+        console.log('GETTER')
+        return this._arr
+    }
+
+    set arr(value) {
+        console.warn('set function has been banned for IMGArray')
+    }
+
+    public push(value: any) {
+        console.log(1111)
+        return this._arr.push(value)
+    }
+
+    public pop() {
+        return this._arr.pop()
+    }
+
+}
+
 
 /**
  * Slider主类
@@ -9,33 +34,36 @@
 class Slider {
 
     // 程序暴露给外部调用的默认配置
-    options: Options = {
-        address: ['http://ov998a2gm.bkt.clouddn.com/lib/pictures/content_1.jpg',
-            'http://ov998a2gm.bkt.clouddn.com/lib/pictures/content_2.jpg',
-            'http://ov998a2gm.bkt.clouddn.com/lib/pictures/bgimg_3.jpg',
-            'http://ov998a2gm.bkt.clouddn.com/lib/pictures/bgimg_4.jpg'],
+    public options: Options = {
+        address: ['http://evink.jscli.org/lib/pictures/content_1.jpg',
+            'http://evink.jscli.org/lib/pictures/content_2.jpg',
+            'http://evink.jscli.org/lib/pictures/bgimg_3.jpg',
+            'http://evink.jscli.org/lib/pictures/bgimg_4.jpg'],
         method: 'random',
         containerId: 'body',
         duration: 1,
         delay: 3000,
         indicatorStyle: 'dot',
         customIndicator: false,
-        indicatorCodes: ['box-shadow:0 0 5px 2px red; opacity:0.8'],
+        indicatorCodes: ['box-shadow:0 0 5px 2px red opacity:0.8'],
         hasClick: false,
         callback: []
-    };
+    }
 
     // 程序初始化时所创造的组件
-    components: Components = {} as any;
+    private components: Components = {} as any
+
+    // imgsConponents
+    private imgsComponents: Array<Components> = []
 
     // 存放组件的样式
-    styles = {
+    private styles = {
         // class
         imgBox: `#slider-img-box {
                     position: relative;
                     width: inherit;
                     height: inherit;
-                    // overflow: hidden;
+                   /* overflow: hidden; */
                 }`,
 
         imgContainers: `#slider-img-box .slider-img-containers {
@@ -49,39 +77,40 @@ class Slider {
         imgs: `#slider-img-box .slider-img-containers .slider-img {
                     width: inherit;
                     height: inherit;
-                }`,
+               }`,
         // id
     }
 
+    private body = document.body
+
     // 构造函数
     constructor(options: Options) {
-        this.options = Object.assign(this.options, options);
-        this.init();
-        this.slide();
+        this.options = Object.assign(this.options, options)
+        this.init()
+        this.slide()
     }
 
     // 初始化图片组件
-    init() {
+    private init() {
 
         // 创建sheet
-        this.generateStyleSheet();
+        this.generateStyleSheet()
 
         // 创建图片元素
-        let imgs: Array<string> | undefined;
-        imgs = this.options.address;
+        let imgs: Array<string> | undefined
+        imgs = this.options.address
         if (imgs === (null || undefined))
-            throw new DOMException('can`t get img elements, Slider.js init failure.');
+            throw new DOMException('can`t get img elements, Slider.js init failure.')
 
-        let body;
         if (this.options.containerId === 'body')
-            body = $('body')[0];
+            this.body = window.document.body
         else
-            body = $('#' + this.options.containerId as string);
-        let imgBox: HTMLDivElement = document.createElement('div');
-        imgBox.id = 'slider-img-box';
-        let i: number = 1;
-        let imgContainers: Array<HTMLDivElement> = new Array();
-        let imgElements: Array<HTMLImageElement> = new Array();
+            this.body = $('#' + this.options.containerId as string)
+        const imgBox: HTMLDivElement = document.createElement('div')
+        imgBox.id = 'slider-img-box'
+        let i: number = 5
+        let imgContainers: Array<HTMLDivElement> = new Array()
+        let imgElements: Array<HTMLImageElement> = new Array()
         // 生成第一遍
         for (let value of imgs) {
             let img = this.generateImg({
@@ -91,38 +120,39 @@ class Slider {
                 classes: ['slider-img'],
                 src: value,
                 farther: imgBox
-            }, i, imgs.length);
-            i++;
-            imgContainers.push(img[0]);
-            imgElements.push(img[1]);
+            }, i, imgs.length)
+            i++
+            imgContainers.push(img[0])
+            imgElements.push(img[1])
+            this.imgsComponents.push(img[0])
         }
         // 记录起始图片
-        this.components.startImg = imgElements[0];
-        this.components.startImgContainer = imgContainers[0];
+        this.components.startImg = imgElements[0]
+        this.components.startImgContainer = imgContainers[0]
 
-        // 生成第二遍
-        for (let value of imgs) {
-            let img = this.generateImg({
-                containerId: `slider-img-container-${i}`,
-                containerClasses: [`slider-img-containers`],
-                id: `slider-img-${i}`,
-                classes: ['slider-img'],
-                src: value,
-                farther: imgBox
-            }, i, imgs.length);
-            i++;
-            imgContainers.push(img[0]);
-            imgElements.push(img[1]);
-        }
+        // // 生成第二遍
+        // for (let value of imgs) {
+        //     let img = this.generateImg({
+        //         containerId: `slider-img-container-${i}`,
+        //         containerClasses: [`slider-img-containers`],
+        //         id: `slider-img-${i}`,
+        //         classes: ['slider-img'],
+        //         src: value,
+        //         farther: imgBox
+        //     }, i, imgs.length)
+        //     i++
+        //     imgContainers.push(img[0])
+        //     imgElements.push(img[1])
+        // }
         // 记录结尾图片
-        this.components.endImg = imgElements[imgElements.length - 1];
-        this.components.endImgContainer = imgContainers[imgContainers.length - 1];
+        this.components.endImg = imgElements[imgElements.length - 1]
+        this.components.endImgContainer = imgContainers[imgContainers.length - 1]
 
-        body.appendChild(imgBox);
+        this.body.appendChild(imgBox)
 
-        this.components.imgBox = imgBox;
-        this.components.imgContainers = imgContainers;
-        this.components.imgs = imgElements;
+        this.components.imgBox = imgBox
+        this.components.imgContainers = imgContainers
+        this.components.imgs = imgElements
 
     }
 
@@ -133,37 +163,37 @@ class Slider {
      * @param imgsLength
      * @returns Array<HTMLDivElement, HTMLImgElement>
      */
-    generateImg(attributes: ImgAttribute, i: number, imgsLength: number): Array<any> {
-        let originalIndex = i;
-        let container: HTMLDivElement = document.createElement('div');
-        container.id = attributes.containerId;
+    private generateImg(attributes: ImgAttribute, i: number, imgsLength: number): Array<any> {
+        let originalIndex = i
+        let container: HTMLDivElement = document.createElement('div')
+        container.id = attributes.containerId
         for (let claz of (attributes.containerClasses)) {
-            container.className += claz + ` `;
+            container.className += claz + ` `
         }
-        let img: HTMLImageElement = document.createElement('img');
-        img.id = attributes.id;
+        let img: HTMLImageElement = document.createElement('img')
+        img.id = attributes.id
         for (let claz of (attributes.classes)) {
-            img.className += claz + ` `;
+            img.className += claz + ` `
         }
-        img.src = attributes.src;
-        container.appendChild(img);
-        attributes.farther.appendChild(container);
+        img.src = attributes.src
+        container.appendChild(img)
+        attributes.farther.appendChild(container)
 
         // 位移
-        let displaceMent;
+        let displaceMent
         if (i >= imgsLength) {
-            i = i - imgsLength - 1;
-            displaceMent = i * 100;
+            i = i - imgsLength - 1
+            displaceMent = i * 100
         } else {
-            displaceMent = (imgsLength - i + 1) * -100;
+            displaceMent = (imgsLength - i + 1) * -100
         }
         let style = ` #slider-img-box #${container.id} {
-                        left: ${displaceMent}%;
-                    }`;
+                        left: ${displaceMent}%
+                    }`
 
         // 添加keyframe
         if (originalIndex != 1) {
-            // i 为1时， 该元素会被动态的移除
+            // i为1时， 该元素会被动态的移除
             let keyframe = ` @keyframes slide-${originalIndex} {
                 0% {
                     left: ${displaceMent}%;
@@ -171,65 +201,72 @@ class Slider {
                 100% {
                     left: ${displaceMent - 100}%;
                 }
-            }`;
-            this.appendToSheet(keyframe);
+            }`
+            this.appendToSheet(keyframe)
         }
 
-        this.appendToSheet(style);
+        this.appendToSheet(style)
 
-        return [container, img];
+        return [container, img]
     }
 
-    generateStyleSheet() {
-        let parent = $('html')[0];
-        let body = $('body')[0];
-        let sheet = document.createElement('style');
-        sheet.id = 'slider-sheet';
-        sheet.className = 'slider-sheet';
-        parent.insertBefore(sheet, body);
-        this.components.sheet = sheet;
-        this.appendToSheet(this.styles.imgBox);
-        this.appendToSheet(this.styles.imgContainers);
-        this.appendToSheet(this.styles.imgs);
-        return sheet;
+    private generateStyleSheet() {
+        const html = $('html')[0]
+        let sheet = document.createElement('style')
+        sheet.id = 'slider-sheet'
+        sheet.className = 'slider-sheet'
+        html.insertBefore(sheet, document.body)
+        this.components.sheet = sheet
+        this.appendToSheet(this.styles.imgBox)
+        this.appendToSheet(this.styles.imgContainers)
+        this.appendToSheet(this.styles.imgs)
+        return sheet
     }
 
-    appendToSheet(style: string) {
-        this.components.sheet.innerHTML += style;
-        this.components.sheet.innerHTML += '\n';
+    private appendToSheet(style: string) {
+        this.components.sheet.innerHTML += style
+        this.components.sheet.innerHTML += '\n'
     }
 
     // 添加进入动画
-    addInAnimations(originalIndex: number) {
+    private addInAnimations(originalIndex: number) {
 
     }
 
     // 添加退出动画
-    addOutAnimations(originalIndex: number) {
+    private addOutAnimations(originalIndex: number) {
 
     }
 
     // 核心逻辑:滑动
     // TODO： 此方法不应该耦合， 传入起始组件， 长度
     slide() {
+        console.log('slide')
         setInterval(() => {
-            this.containerSlideEvent();
-        }, 1000);
+            console.log(1)
+            this.containerSlideEvent()
+        }, 1000)
     }
 
     containerSlideEvent() {
-        let containers = this.components.imgContainers;
-        let imgLength = containers.length;
-        let i = 0;
-        for (let container of containers) {
-            let id = container.id;
-            this.appendToSheet(`#${id}{
-                animation: slide-${i + 1} 1s;
-                animation-fill-mode: forwards;
-            }`);
+        let containers = this.components.imgContainers
+        console.log(containers[0])
 
-            i++;
-        }
+        // let imgLength = containers.length
+        // let i = 0
+        // for (let container of containers) {
+        //     let id = container.id
+        //     this.appendToSheet(`#${id}{
+        //         animation: slide-${i + 1} 1s;
+        //         animation-fill-mode: forwards;
+        //     }`)
+
+        //     i++
+        // }
+
+    }
+
+    private imgsOnChange(img: Components) {
 
     }
 
@@ -238,7 +275,13 @@ class Slider {
 
 
 // @example
-let opts = {
-};
-let s = new Slider(opts);
+let opts: { EvinK?: any } = {
+}
+
+let arr = []
+// let s = new Slider(opts)
+
+
+
+
 
